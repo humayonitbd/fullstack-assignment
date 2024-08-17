05cafa5f360e3bb15e02217f59a786f17be58228
humayonitbd <humayonforid44@gmail.com>
2024-08-17 14:07:33 +0600

    help page desing

--- frontend/src/pages/HelpPage/HelpPage.tsx
+++ frontend/src/pages/HelpPage/HelpPage.tsx
@@ -1,11 +1,36 @@
-
+import { FaArrowRight } from "react-icons/fa6";
 
 const HelpPage = () => {
-    return (
-        <div>
-            help page
+  const searchHandler = (e: any) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const searchText = form.search.value;
+    console.log("searchText", searchText);
+    form.reset();
+  };
+  return (
+    <div>
+      <div className="bg-[#DADBF0] h-80 flex justify-center items-center  ">
+        <div className="text-center">
+          <h2 className="text-6xl font-semibold text-slate-900 mb-5">
+            How can we help?
+          </h2>
+          <div>
+            <form onClick={searchHandler}>
+              <input
+                type="text"
+                name="search"
+                className="py-3 px-2 text-lg border border-slate-900 rounded w-[600px] "
+                placeholder="Search here..."
+              />
+              <FaArrowRight type="submit" className="inline -ml-9 text-xl cursor-pointer" />
+            </form>
+          </div>
         </div>
-    );
+      </div>
+      {/* api get section  */}
+    </div>
+  );
 };
 
 export default HelpPage;
--- frontend/src/pages/SharedPage/Footer/Footer.tsx
+++ frontend/src/pages/SharedPage/Footer/Footer.tsx
@@ -1,9 +1,67 @@
-
+import {NavLink} from 'react-router-dom'
 const Footer = () => {
     return (
-        <div>
-            footer
+      <div className="bg-slate-900 py-10 px-20 text-slate-200">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-10">
+          <div>
+            <h4 className="text-lg font-bold mb-5">Abstract</h4>
+            <NavLink to="/" className="hover:underline">
+              <p className="mb-1">Branches</p>
+            </NavLink>
+          </div>
+          <div>
+            <h4 className="text-lg font-bold mb-5">Resources</h4>
+            <NavLink to="/" className="hover:underline">
+              <p className="mb-1">Blog</p>
+            </NavLink>
+            <NavLink to="/" className="hover:underline">
+              <p className="mb-1">Help Center</p>
+            </NavLink>
+            <NavLink to="/" className="hover:underline">
+              <p className="mb-1">Release Notes</p>
+            </NavLink>
+            <NavLink to="/" className="hover:underline">
+              <p className="mb-1">Status</p>
+            </NavLink>
+          </div>
+          <div>
+            <h4 className="text-lg font-bold mb-5">Community</h4>
+            <NavLink to="/" className="hover:underline">
+              <p className="mb-1">Twitter</p>
+            </NavLink>
+            <NavLink to="/" className="hover:underline">
+              <p className="mb-1">LinkedIn</p>
+            </NavLink>
+            <NavLink to="/" className="hover:underline">
+              <p className="mb-1">Facebook</p>
+            </NavLink>
+            <NavLink to="/" className="hover:underline">
+              <p className="mb-1">Dribble</p>
+            </NavLink>
+            <NavLink to="/" className="hover:underline">
+              <p className="mb-1">Podcast</p>
+            </NavLink>
+          </div>
+          <div>
+            <h4 className="text-lg font-bold mb-5">Company</h4>
+            <NavLink to="/" className="hover:underline">
+              <p className="mb-1">About Us</p>
+            </NavLink>
+            <NavLink to="/" className="hover:underline">
+              <p className="mb-1">Creers</p>
+            </NavLink>
+            <NavLink to="/" className="hover:underline">
+              <p className="mb-1">Legal</p>
+            </NavLink>
+            <p className="font-bold mb-1">Cnntact Us</p>
+            <p className="text-sm">info@abstract.com</p>
+          </div>
         </div>
+        <hr className="border-t-2 text-slate-200 mt-10" />
+        <div className="text-center mt-2">
+          @ Copyright 2024 Abstract Studio Design, Inc. All rights reserved.
+        </div>
+      </div>
     );
 };
 
